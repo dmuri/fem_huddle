35ed192da1806407d3aab97e0802edb4a71590e3
dmuri <72742970+dmuri@users.noreply.github.com>
2023-08-07 14:03:31 +0200

    completed project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -6,11 +7,35 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      'sm': '640px',   // modify this for 'sm' breakpoint
+      'md': '920px',   // modify this for 'md' breakpoint
+      'lg': '1080px',  // modify this for 'lg' breakpoint
+      'xl': '1270px',  // modify this for 'xl' breakpoint
+      '2xl': '1563px', // modify this for '2xl' breakpoint
+    },
     extend: {
+      gridTemplateColumns: {
+        '60-40': '60% 40%',
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'mobile': "url('/images/bg-mobile.svg')",
+        'desktop': "url('/images/bg-desktop.svg')",
+        'illustration': "url('/images/illustration-mockups.svg')",
+      },
+      fontWeight: {
+        'normal': 400,
+        'semibold': 600,
+        'bold': 700,
+      },
+      fontFamily: {
+        'sans': ['Open Sans', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: {
+          violet: 'hsl(257, 40%, 49%)',
+          softmagenta: 'hsl(300, 69%, 71%)',
+        },
       },
     },
   },
